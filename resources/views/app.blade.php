25ca00ccfa65cc5072556bbf0e0e2312871c906c
Lionel AZ <lionel.aziangbe@gmail.com>
2026-04-27 00:45:54 +0000

    Update app icons to use Scolaris logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,9 +12,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/img/scolaris_logo.png" type="image/png">
+        <link rel="apple-touch-icon" href="/img/scolaris_logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
