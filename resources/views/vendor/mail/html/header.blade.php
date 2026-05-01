359115f278efce6bed44a8ab1bed3c3edb435851
Lionel AZ <lionel.aziangbe@gmail.com>
2026-05-01 21:35:39 +0000

    Update mail template branding: adjust logo styling, replace footer text with "SCOLARIS FINANCE", and delete unused temporary file.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ config('app.url') }}/img/scolaris_logo.png" class="logo" alt="Scolaris Logo">
+<img src="{{ config('app.url') }}/img/scolaris_logo.png" class="logo" alt="Scolaris Finance" style="max-height: 75px; width: auto;">
 </a>
 </td>
 </tr>
